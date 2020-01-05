c96d9b84d441376b00572618a56315b12fa6f79b
Chris McKenzie <kristopolous@yahoo.com>
2020-01-05 22:55:52 +0000

    state equity between driver and passenger

--- proxy.php
+++ proxy.php
@@ -1,9 +1,16 @@
 <?
 // this is so we can do session variables and have a pass thru to the waivescreen api
-session_start()
+session_start();
 $action = $_GET['action'];
 if($action === 'request') {
-  $id = $_GET['id'];
-  $goober_id = file_get_contents("http://waivescreen.com/api/request?id=$id");
-  var_dump($goober_id);
+  $car = $_GET['id'];
+  $goober_id = file_get_contents("http://waivescreen.com/api/request?id=$car");
+  if($goober_id) {
+    $_SESSION['id'] = $goober_id;
+    $_SESSION['car'] = $car;
+    echo $goober_id;
+  } else {
+    echo 'false';
+  }
+  session_commit();
 }
